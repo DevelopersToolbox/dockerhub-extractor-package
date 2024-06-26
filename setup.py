32aa73419a3ab5e5d9e71b916d563cf831148ddc
Wolf <wolf@tgwolf.com>
2024-06-26 13:57:52 +0100

    split requirements

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", 'r', encoding='UTF-8') as f:
 
 setup(
     name='wolfsoftware.dockerhub-extractor',
-    version='0.1.0',
+    version='0.1.1',
     author='Wolf Software',
     author_email='pypi@wolfsoftware.com',
     description='Extract container information for a given user in DockerHub.',
